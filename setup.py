846f8603082042a7a8b90028c956ac6c283b23a5
DY282 <2821963166@qq.com>
2025-01-15 17:17:58 +0800

    增加打包

---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("DataVerification.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="data_verification",  # 包名
+    version="0.0.1",  # 包版本号
+    author="39",  # 作者名
+    author_email="DY39project@outlook.com",  # 联系方式
+    description="数据检验的工具",  # 包的简述
+    long_description=long_description,  # 包的详细介绍
+    long_description_content_type="text/markdown",
+    url="https://github.com/yanshi121/DataVerification",  # 项目地址
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: POSIX :: Linux",
+    ],
+    python_requires='>=3.5',  # 对python的最低版本要求
+    install_requires=["hashlib", "types", "re", "asyncio", "geopandas", "shapely", "decimal", "datetime"]  # 项目依赖的其他库
+)
